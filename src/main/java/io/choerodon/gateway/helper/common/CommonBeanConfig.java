9373a38a3ac9e2ba3177594dd232f175d39278d9
flyleft <jcalaz@163.com>
2018-09-29 14:13:22 +0800

    [IMP] 去除了EtagFilter

--- src/main/java/io/choerodon/gateway/helper/common/CommonBeanConfig.java
+++ src/main/java/io/choerodon/gateway/helper/common/CommonBeanConfig.java
@@ -1,24 +1,15 @@
 package io.choerodon.gateway.helper.common;
 
-import javax.servlet.Filter;
-
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 /**
  * @author flyleft
- * @date 2018/4/17
  */
 @Configuration
 public class CommonBeanConfig {
-
-    @Bean
-    public Filter shallowEtagHeaderFilter() {
-        return new ShallowEtagHeaderFilter();
-    }
 
     @Bean("oauth_rest")
     @LoadBalanced
